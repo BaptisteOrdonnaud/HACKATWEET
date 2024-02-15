738e3a1738420951160173c832d84fe72bc83ec7
Baptiste Ordonnaud <baptisteordonnaud@gmail.com>
2024-02-15 16:02:01 +0100

    ajout composant tweet

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -5,10 +5,44 @@ import Tweet from './Tweet'
 
 function Home() {
   return (
-    <div>
-      <LastTweets />
-      <Tweet />
-      <Trends />
+    <div className={styles.homeContainer}>
+
+    {/* PARTIE GAUCHE */}
+
+      <div className={styles.leftContainer}>
+          <img className={styles.logoTwitter} src="Logo_Twitter.png" alt="Logo" />
+      <div className={styles.profilContainer}>
+          <div className={styles.userInformation}>
+              <img className={styles.userAvatar} src="avatar.png" alt="Photo de profil de l'utilisateur" />
+            <div>
+              <h3 className={styles.userFirstname}>John</h3>
+              <h4 className={styles.userUsername}>@JohnCena</h4>
+            </div>
+          </div>
+          <button className={styles.logoutBtn}>Logout</button>
+        </div>
+      </div>
+
+
+  {/* PARTIE MILIEU */}
+
+
+      <div className={styles.middleContainer}>
+          <div className={styles.tweetCompoContainer}>
+            <h2 className={styles.tweetContainerTitle}>Home</h2>
+          <Tweet />
+
+          </div>
+        {/* <LastTweets /> */}
+      </div>
+      
+
+
+        {/* PARTIE DROITE */}
+      
+      <div className={styles.rightContainer}>
+       <Trends />
+      </div>
     </div>
   );
 }
--- frontend/components/Tweet.jsx
+++ frontend/components/Tweet.jsx
@@ -2,8 +2,14 @@ import styles from '../styles/Home.module.css';
 
 function Tweet() {
     return (
-        <div>
-            TWEET
+        <div className={styles.tweetContainer}>
+        
+                 <input className={styles.tweetInput} type="text" placeholder="What's up?"  maxlength="280" />
+
+            <div className={styles.btnContainer}>
+                <span>0/280</span>
+                <button className={styles.tweetBtn}>Tweet</button>
+            </div>
         </div>
     );
 }
