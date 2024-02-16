88b59403e674436613f8c5edc27ede4f60f3402d
Baptiste Ordonnaud <baptisteordonnaud@gmail.com>
2024-02-16 08:59:28 +0100

    finalisation du css

--- frontend/components/LastTweets.jsx
+++ frontend/components/LastTweets.jsx
@@ -2,9 +2,19 @@ import styles from '../styles/Home.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
+import { useState } from 'react';
+
+
 
 
 function LastTweets() {
+
+    const [nbLike, setNbLike] = useState(0);
+
+    const handleClick = () => {
+        setNbLike(nbLike + 1);
+      };
+
     return (
         <div className={styles.lastTweets}>
             <div className={styles.infoUserTweet}>
@@ -13,10 +23,10 @@ function LastTweets() {
                 <h4 className={styles.userUsernameTweet}>@JohnCena</h4>
                 <span className={styles.timeTweet}>a few seconds</span>
             </div>
-            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un <span>#imprimeur</span> anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.</p>    
+            <p className={styles.tweetContent}>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un <span className={styles.hashtag}>#imprimeur</span> anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.</p>    
             <div>
-                <FontAwesomeIcon icon={faHeart} /> <span>0</span>
-                <FontAwesomeIcon icon={faTrashCan} />
+                <FontAwesomeIcon onClick={() => handleClick()} icon={faHeart} className={styles.iconHeart}/> <span>{nbLike}</span>
+                <FontAwesomeIcon icon={faTrashCan} className={styles.iconTrash} />
             </div>
         </div>
     );
--- frontend/components/Trends.jsx
+++ frontend/components/Trends.jsx
@@ -3,7 +3,23 @@ import styles from '../styles/Home.module.css';
 function Trends() {
     return (
         <div>
-            TRENDS
+            <h2 className={styles.tweetContainerTitle}>Trends</h2>
+            <div className={styles.trends}>
+                <div className={styles.trendsContent}>
+                    <div className={styles.trend}>
+                    <h3 className={styles.trendTitle}>#hackatweet</h3>
+                    <p className={styles.trendNumber}>2 Tweets</p>
+                    </div>
+                    <div className={styles.trend}>
+                    <h3 className={styles.trendTitle}>#first</h3>
+                    <p className={styles.trendNumber}>2 Tweet</p>
+                    </div>
+                    <div className={styles.trend}>
+                    <h3 className={styles.trendTitle}>#prout</h3>
+                    <p className={styles.trendNumber}>100 tweets</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
